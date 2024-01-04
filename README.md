9d293122d505768517cfdb805eca0167a57cd7d7
zephrous <danielpoh3131@gmail.com>
2024-01-04 20:46:55 +0800

    Building myst3r10us website.:

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 "# myst3r10us.github.io" 
 
 "# undergoing project, building myst3r10us website for CTF purposes."
+
+
+links used:
+https://tailwindcss.com/docs/guides/nextjs
---
+++ my-project/pages/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const index = () => {
+  return (
+    <div className="text-9xl text-gray-800">
+      myst3r10us
+    </div>
+  )
+}
+
+export default index
