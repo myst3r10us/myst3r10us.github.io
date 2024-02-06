decf0cf34f8b02d634fb8e6665e9ae0a6102e263
zephrous <danielpoh3131@gmail.com>
2024-02-06 14:46:20 +0800

    about us

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,46 +1,56 @@
-import React from 'react'
-import { Navigationbar } from "@/components/(navbar)/Navigationbar";
+import React from 'react';
+import { Navigationbar } from '@/components/(navbar)/Navigationbar';
 
-const components: {title: string; href: string; description: string }[] = [
-    {
-        title: "What is myst3r10us?",
-        href: "",
-        description:
-            "A Malaysian-based CTF team that starts in November 2023.",
-    },
-    {
-        title: "Why myst3r10us?",
-        href: "",
-        description:
-            "It started from 'Mysterious Three Flaggers' and it shortened to myst3r10us.",
-    },
-    {
-        title: "Our CTFTime Team Profile",
-        href: "https://ctftime.org/team/277613",
-        description:
-            "Check out how many CTFs we have participated in!",
-    },
-    {
-        title: "Join myst3r10us",
-        href: "",
-        description: "Want to join the team? Contact us for further information!",
-    },
+const components: { title: string; href: string; description: string }[] = [
+  { 
+    title: 'What is myst3r10us?',
+    href: '',
+    description:
+      'A Malaysian-based CTF team that started in November 2023. We are a group of cybersecurity enthusiasts who are passionate about solving challenges and learning new things. And here we are.',
+  },
+  {
+    title: 'Why myst3r10us?',
+    href: '',
+    description:
+      "It started from 'Mysterious Three Flaggers' and over time, it shortened to myst3r10us. We are also known as M3Flaggers, my$t3r10u$. my5t3r10u5..",
+  },
+  {
+    title: 'Our CTFTime Team Profile',
+    href: 'https://ctftime.org/team/277613',
+    description:
+      'Check out how many CTFs we have participated in! Do not expect good things lol.',
+  },
+  {
+    title: 'Join myst3r10us?!?',
+    href: '',
+    description:
+      'Unfortunately, we do not plan to invite more people to join the team, but keep in mind that we will recruit new team members in the near future. For now, we are open to collaboration and partnership. Feel free to reach out to us if you have any interesting ideas or projects to work together.',
+  },
 ];
 
 const About = () => {
   return (
-    <div>
+    <div className=" bg-slate-900 min-h-screen">
       <Navigationbar />
-      <h1 className="text-4xl font-bold">About myst3r10us</h1>
-      {components.map((component, index) => (
-        <div key={index}>
-          <h2>{component.title}</h2>
-          <p>{component.description}</p>
-          {component.href && <a href={component.href}>Learn More</a>}
-        </div>
-      ))}
+      <div className="container mx-auto py-8">
+        <h1 className="text-5xl font-bold mb-16 text-center">About myst3r10us</h1>
+        {components.map((component, index) => (
+          <div key={index} className="mb-8">
+            <h2 className="text-2xl font-bold mb-2">{component.title}</h2>
+            <p className="  text-cyan-500 mb-4">{component.description}</p>
+            {component.href && (
+              <a
+                href={component.href}
+                className="text-blue-500 hover:underline"
+              >
+                View CTF Team Profile on CTFTime
+              </a>
+            )}
+          </div>
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
