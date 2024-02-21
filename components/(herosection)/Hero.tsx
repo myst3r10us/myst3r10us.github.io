849ff65e71883b4ed3b36fa529f1c6f9bced987c
zephrous <danielpoh3131@gmail.com>
2024-02-21 13:52:39 +0800

    some progress

--- components/(herosection)/Hero.tsx
+++ components/(herosection)/Hero.tsx
@@ -20,12 +20,21 @@ export const HeroSection: React.FC = () => {
                 justifyContent: 'space-between',
             }}
         >
-            <div>
-                <h1 style={{ fontSize: '48px', marginBottom: '20px' }}>Hello, Hackers.</h1>
-                <h3 style={{ fontSize: '24px', marginBottom: '10px' }}>Welcome to myst3r10us!</h3>
-                <p style={{ fontSize: '16px', marginBottom: '30px' }}>Nothing to see.</p>
+            <div className="font-DMS">
+                <h1 style={{ fontSize: '10em', marginBottom: '10px', marginRight: '50px', top: "0", left: "50px", position: "relative" }}>
+                    Hello, 
+                    <span
+                        style={{
+                            content: "''",
+                        }}
+                    className="bg-clip-text text-transparent bg-gradient-to-r from-emerald-400 to-cyan-400">&nbsp;Hackers</span>
+                </h1>
+                <h3 style={{ fontSize: '3em', marginBottom: '10px', lineHeight: '1.5', top: "0", left: "50px", position: "relative" }} className=" tracking-widest leading-loose bg-clip-text text-transparent bg-gradient-to-r from-sky-500 to-indigo-500">Welcome to myst3r10us</h3>
+                <p style={{ fontSize: '1.25em', marginBottom: '30px', fontStyle: 'italic', top: "0", left: "50px", position: "relative" }} className="text-center tracking-widest bg-clip-text text-transparent bg-gradient-to-r from-blue-600 to-violet-600">We like to solve CTF Challenges</p>
             </div>
-            <Image src={avatar} alt="Side Image" width={400} height={400} />
+            <div style={{ position: 'relative', left: '-100px' }}>
+                <Image src={avatar} alt="Side Image" width={400} height={400} style={{ animation: 'slide-in 5s forwards' }} />
+            </div>
         </div>
     );
 };
