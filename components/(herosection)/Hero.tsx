fabc598893c81f1e25afe3197fd4dd9536336ff9
zephrous <danielpoh3131@gmail.com>
2024-02-21 11:06:24 +0800

    update

--- components/(herosection)/Hero.tsx
+++ components/(herosection)/Hero.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Image from 'next/image';
+import bg from '../assets/background.png';
+import avatar from '../assets/avatar.jpg';
 
 export const HeroSection: React.FC = () => {
     return (
         <div
             style={{
-                backgroundImage: `url('./assets/background.png')`,
+                backgroundImage: bg.toString(),
                 backgroundSize: 'cover',
                 backgroundPosition: 'center',
                 backgroundRepeat: 'no-repeat',
@@ -20,7 +22,7 @@ export const HeroSection: React.FC = () => {
                 <h3 style={{ fontSize: '24px', marginBottom: '10px' }}>Subheading</h3>
                 <p style={{ fontSize: '16px', marginBottom: '30px' }}>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
             </div>
-            <Image src="/assets/avatar.jpg" alt="Side Image" width={300} height={300} />
+            <Image src={avatar} alt="Side Image" width={300} height={300} />
         </div>
     );
 };
