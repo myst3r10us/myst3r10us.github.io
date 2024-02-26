ca539aa13ced6d61cf63c7fa59f9c69ef5738dbb
zephrous <danielpoh3131@gmail.com>
2024-02-26 15:42:51 +0800

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,21 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-
+      <head>
+        <link rel="icon" href="./avatar.jpg" sizes="any" />
+        <link
+          rel="icon"
+          href="/icon?<generated>"
+          type="image/<generated>"
+          sizes="<generated>"
+        />
+        <link
+          rel="apple-touch-icon"
+          href="/apple-icon?<generated>"
+          type="image/<generated>"
+          sizes="<generated>"
+        />
+      </head>
       <body className={inter.className}>
         <ThemeProvider
           attribute="class"
