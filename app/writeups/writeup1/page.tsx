5ac4b0cddc139e279a342759421e1d14605c00e6
zephrous <danielpoh3131@gmail.com>
2024-02-29 10:57:12 +0800

    update

--- app/writeups/writeup1/page.tsx
+++ app/writeups/writeup1/page.tsx
@@ -1,13 +1,12 @@
-import Footer from '@/components/(footer)/Footer';
-import Navigationbar from '@/components/(navbar)/Navigationbar';
+import { FooterPage } from '@/components/(footer)/Footer';
+import { Navigationbar } from '@/components/(navbar)/Navigationbar';
 import React from 'react';
 
 const WriteupOne = () => {
     return (
         <div>
-            <h1>Schrödinger&apos;s Cat 2</h1>
-            <p>Miscellaneous category from SKRCTF.</p>
-            <p>Flag: <code>SKR{}</code></p>
+            <Navigationbar/>
+            <FooterPage/>   
         </div>
     );
 };
