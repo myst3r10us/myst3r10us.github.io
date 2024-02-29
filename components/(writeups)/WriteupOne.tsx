1b6b5886742159d2109f2884ebad6998e1e592eb
zephrous <danielpoh3131@gmail.com>
2024-02-29 12:16:32 +0800

    update

--- components/(writeups)/WriteupOne.tsx
+++ components/(writeups)/WriteupOne.tsx
@@ -13,9 +13,9 @@ const WriteupOnePage = () => {
                 <p className="text-sm text-gray-500">Last updated: February 29, 2024 11:30 AM</p>
                 <p className="ml-42  text-sm text-gray-500">Author: z3phr0us</p>
             </div>
-            <p className=" text-justify text-wrap text-lg w-3/5 h-48">
+            <p className=" text-justify text-wrap text-lg font-DMS w-3/5 h-48">
                 One of my favorite CTF challenges in SKRCTF and I am so happy that I could solve this fun challenge.
-                Before that, I suggest you do Schrödinger&apos;s Cat 1 first to get an idea of this challenge and my solution.
+                Before that, I suggest you do Schrödinger&apos;s Cat 1 first to get an idea of this 2nd challenge and my solution (Challenge 1: <Link href="https://skrctf.me/challenges#Schr%C3%B6dinger's%20Cat" target="_blank">Here</Link>).
                 <br/><br/>How do I find out the answer/flag? It is actually simple but quite a lengthy process.
             </p>
 
