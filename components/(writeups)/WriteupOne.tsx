41dfb6a03d4bd679460c19cf93bbe461e3461ad3
zephrous <danielpoh3131@gmail.com>
2024-02-29 11:53:30 +0800

    update

--- components/(writeups)/WriteupOne.tsx
+++ components/(writeups)/WriteupOne.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const WriteupOnePage = () => {
     return (
-        <div>
+        <div className="mb-8 mt-8">
             <h1>Schrödinger&apos;s Cat 2</h1>
             <p>Miscellaneous category from SKRCTF.</p>
             <p>Flag: <code>SKR{}</code></p>
